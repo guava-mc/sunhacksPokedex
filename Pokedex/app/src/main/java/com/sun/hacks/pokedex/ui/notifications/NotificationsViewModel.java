da20369df2b7f7dd6e22810568bb22d5acc60343
Mason Cole <mcole65@gatech.edu>
2021-10-02 17:30:59 -0700

    update icons and titles

--- Pokedex/app/src/main/java/com/sun/hacks/pokedex/ui/notifications/NotificationsViewModel.java
+++ Pokedex/app/src/main/java/com/sun/hacks/pokedex/ui/notifications/NotificationsViewModel.java
@@ -10,7 +10,7 @@ public class NotificationsViewModel extends ViewModel {
 
     public NotificationsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("Catch more pokemon");
     }
 
     public LiveData<String> getText() {
